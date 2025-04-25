229b39547e6b4e26e93991c2dba3928ee727008a
weixiao2925 <weixiao2925@gmail.com>
2025-04-25 09:45:28 +0800

    fix: 清除旧的后端目录并重新添加干净结构

---
+++ cvItem01/src/main/java/org/example/cvitme01/CvItem01Application.java
@@ -0,0 +1,13 @@
+package org.example.cvitme01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvItem01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvItem01Application.class, args);
+    }
+
+}
