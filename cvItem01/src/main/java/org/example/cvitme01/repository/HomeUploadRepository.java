81891c26136c6ee79c043726f675f6c79f2c0ae9
weixiao2925 <weixiao2925@gmail.com>
2025-05-09 16:54:39 +0800

    feat: 删除不必要的Repository

--- cvItem01/src/main/java/org/example/cvitme01/repository/HomeUploadRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.cvitme01.repository;
-
-import org.babyfish.jimmer.spring.repository.JRepository;
-import org.example.cvitme01.entity.dto.Reading;
-
-public interface HomeUploadRepository extends JRepository<Reading, Long> {
-
-}
